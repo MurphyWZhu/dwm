a4558d583d14eac7dff2feeddc0e50b81ac2d346
Murphy.W.Zhu <Murphy.W.Zhu@aliyun.com>
2020-08-04 21:37:13 +0800

    error : roficmd line add ";"

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *roficmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-theme", "/usr/share/rofi/themes/Pop-Dark.rasi", "-icon-theme", "Qogir", "-show-icons", "-font", "文泉驿等宽微米黑 Italic 18", NULL }
+static const char *roficmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-theme", "/usr/share/rofi/themes/Pop-Dark.rasi", "-icon-theme", "Qogir", "-show-icons", "-font", "文泉驿等宽微米黑 Italic 18", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
